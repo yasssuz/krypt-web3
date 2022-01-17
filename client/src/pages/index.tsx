69a86f2d6061f2e700c8047290d4900b561d197c
Karim Yassine <karimwebdevelopment@gmail.com>
2022-01-16 23:20:37 -0300

    add navbar and hero

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from "next";
 
+import Navbar from "../components/common/navbar";
+import Hero from "../components/home/hero";
+
 const Home: NextPage = () => {
-  return <h1>hello world</h1>;
+  return (
+    <main className='min-h-screen'>
+      <section className='gradient-bg-welcome'>
+        <Navbar />
+        <Hero />
+      </section>
+    </main>
+  );
 };
 
 export default Home;
