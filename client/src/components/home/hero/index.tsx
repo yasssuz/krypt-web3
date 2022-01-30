def3b5f929b57ff8acd75b58def99a880d21f90d
Karim Yassine <karimwebdevelopment@gmail.com>
2022-01-29 21:25:47 -0300

    add form

--- client/src/components/home/hero/index.tsx
+++ client/src/components/home/hero/index.tsx
@@ -1,19 +1,26 @@
-import { AiFillPlayCircle } from "react-icons/ai";
+import { AiFillPlayCircle, AiOutlineConsoleSql } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
+import Input from "../../common/input";
+import Loader from "../../common/loader";
+import { useState } from "react";
 
 const Hero = (): JSX.Element => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleConnectWallet = (): void => {};
+
+  const handleSubmit = (): void => {};
 
   return (
     <section className='flex w-full justify-center items-center'>
-      <div className='flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px4'>
-        <div className='flex flex-1 justify-start flex-col md:mr-10'>
+      <div className='flex mf:flex-row flex-col items-start justify-between mf:p-20 py-12 px4'>
+        <div className='flex flex-1 justify-start flex-col mf:mr-10'>
           <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
             Send Crypto
             <br /> across the world
           </h1>
-          <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
+          <p className='text-left mt-5 text-white font-light mf:w-9/12 w-11/12 text-base'>
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             Krypt
           </p>
@@ -37,7 +44,7 @@ const Hero = (): JSX.Element => {
             <span className='rounded-bl-2xl min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'>
               Web 3.0
             </span>
-            <span className=' min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'>
+            <span className='min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'>
               Low fees
             </span>
             <span className='rounded-br-2xl min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white'>
@@ -45,7 +52,7 @@ const Hero = (): JSX.Element => {
             </span>
           </div>
         </div>
-        <div className='flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10'>
+        <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
           <div className='p-3 justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism'>
             <div className='flex justify-between flex-col w-full h-full'>
               <div className='flex justify-between items-start'>
@@ -62,6 +69,57 @@ const Hero = (): JSX.Element => {
               </div>
             </div>
           </div>
+          <form
+            className='p-5 sm:w-96  w-full flex flex-col justify-start items-center blue-glassmorphism'
+            onSubmit={handleSubmit}
+          >
+            <Input
+              placeholder='Address To'
+              name='addressTo'
+              id='addressTo'
+              type='text'
+              step='0.0001'
+              onChange={() => console.log("hello")}
+              className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+            />
+            <Input
+              placeholder='Amount (ETH)'
+              name='amount'
+              id='amount'
+              type='number'
+              onChange={() => console.log("hello")}
+              className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+            />
+            <Input
+              placeholder='Keyword (Gif)'
+              name='keyword'
+              id='keyword'
+              type='text'
+              onChange={() => console.log("hello")}
+              className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+            />
+            <Input
+              placeholder='Enter Message'
+              name='message'
+              id='message'
+              type='text'
+              onChange={() => console.log("hello")}
+              className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
+            />
+
+            <div className='h-[1px] w-full bg-gray-400 my-2' />
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <button
+                type='submit'
+                className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer'
+              >
+                {" "}
+                Send Now
+              </button>
+            )}
+          </form>
         </div>
       </div>
     </section>
